5dfded2c5bbd252444bc3bc88114ec39e3c610e0
Azizbek Rasulmetov <azizbek.rasulmetov2000@gmail.com>
2021-06-12 21:13:28 +0500

    project setup

---
+++ app/src/main/java/com/example/mysearchphotosapp/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+ package com.example.mysearchphotosapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mysearchphotosapp.R
+
+ class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
